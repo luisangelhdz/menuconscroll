63515b7399771c10f0f8d3a5998b110f749fddf2
Luis Angel Hernandez <43392488+luisangelhdz@users.noreply.github.com>
2018-12-20 09:28:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>efecto scroll en menu</title>
+	<link rel="stylesheet" href="style/index_style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+	<script src='js/script.js'></script>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<a href=''><li>Inicio</li></a>
+				<a href=''><li>Videos</li></a>
+				<a href=''><li>Im&aacute;genes</li></a>
+			</ul>
+			<ul>
+				<a href=''><li>Youtube</li></a>
+				<a href=''><li>Nosotros</li></a>
+				<a href=''><li>Contacto</li></a>
+			</ul>
+			<img id='logo' src='pictures/logo.png'>
+		</nav>
+	</header>
+
+	<section id='banner'>
+	</section>
+	<section id='body'>
+		<p id="parra"  class="animated  bounceInDown "style="display:  none;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<img id="imagen"src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/19d0ef24-e6b0-41ef-ad12-ea7476bc5374/d4l4c0p-5ae3a24f-c01c-43aa-87bf-d141a4f7e3a6.png" alt="">
+
+	</section>
+
+</body>
+</html>
